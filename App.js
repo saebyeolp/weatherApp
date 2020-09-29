3881e74f5597aa24014b9147f225f7df9a2a50a0
saebyeolp <saebyeolpak@gmail.com>
2020-09-28 20:38:59 -0700

    third

--- App.js
+++ App.js
@@ -6,6 +6,14 @@ import Form from './components/Form'
 
 
 const App = () => {
+
+  // const images = {
+  //   Clear: 'http://ayay.co.uk/mobiles/weather/strange/northern-lights.jpg',
+  //   Clouds:
+  //     'https://www.princeton.edu/sites/default/files/styles/full_2x/public/images/2018/01/clouds-19.jpg?itok=7jputHX1',
+  //   Rain:
+  //     'https://i.pinimg.com/736x/54/59/d7/5459d741279e8d72661990f52774473f--cell-phone-wallpapers-gif-photos.jpg'
+  // }
   
   const API_KEY = 'c8c4865b1ee49f77a87780ffabb644b3'
 
@@ -27,6 +35,11 @@ const App = () => {
 
   return (
     <View style={styles.container}>
+
+      {/* <Image
+        style={{ position: 'absolute', width: '100%', height: '100%' }}
+        source={{ uri: images[weather] }}
+      /> */}
 
       <Form
         onSubmit={city}
